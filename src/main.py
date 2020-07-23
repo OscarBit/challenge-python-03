87152d83f83a49752b6f6f026e031d293429fa7c
Calamar <oebolanoso@unal.edu.co>
2020-07-23 11:28:50 -0500

    Challenge complete!

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
-
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        raw_secret = f.read()
+    print('The secrect key is: ', "".join(re.findall(r'[a-z]',raw_secret)))
 
 if __name__ == '__main__':
     run()
